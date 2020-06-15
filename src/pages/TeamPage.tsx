bfcf077801c989f9b6f3ddc73e4465ca5fab8736
Liam Dawson <liam@ldaws.com>
2020-06-15 13:32:13 +1000

    Add waiting music

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Team } from "../teams";
 import { HazeBackground, TeamHeader, TeamOrder } from "../components";
+import { FiMusic } from "react-icons/fi";
 
 interface IProps {
   teams: Array<Team>;
@@ -10,20 +11,47 @@ interface IProps {
 
 export const TeamPage: React.FC<IProps> = ({ teams, dayNumber }) => {
   const { teamId } = useParams();
+  const [playWaitingMusic, setPlayWaitingMusic] = useState(false);
 
-  const team = teams.find(t => t.id === teamId);
+  const team = teams.find((t) => t.id === teamId);
 
   let content = <></>;
 
   if (team) {
-    const presentMembers = team.members.filter(member => !member.away);
+    const presentMembers = team.members.filter((member) => !member.away);
 
     content = (
-      <div style={{ color: "var(--team-color)" }}>
-        <HazeBackground />
-        <TeamHeader team={team} />
-        <TeamOrder dayNumber={dayNumber} members={presentMembers} />
-      </div>
+      <>
+        <div style={{ color: "var(--team-color)" }}>
+          <HazeBackground />
+          <TeamHeader team={team} />
+          <TeamOrder dayNumber={dayNumber} members={presentMembers} />
+        </div>
+        <p style={{ paddingTop: "2ex" }}>
+          <button
+            onClick={() => setPlayWaitingMusic(!playWaitingMusic)}
+            style={{
+              background: "none",
+              border: "none",
+              font: "inherit",
+              color: "inherit",
+              padding: "2ex 1em",
+              cursor: "pointer",
+            }}
+          >
+            <FiMusic /> Toggle waiting music <FiMusic />
+          </button>
+          {playWaitingMusic ? (
+            <audio
+              autoPlay={true}
+              loop={true}
+              src="/waiting/girl-from-ipanema.ogg"
+            >
+              Aww, your browser doesn't support this.
+            </audio>
+          ) : null}
+        </p>
+      </>
     );
   }
 
